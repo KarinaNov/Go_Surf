bfcc25a1b9669743c3e7668852e09565f5c64f5b
KarinaNov <flavouredhtc@gmai.com>
2020-05-18 08:30:51 +0200

    add main block + serf block

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt=""></img>',
+        asNavFor: '.slider-dotshead'
+      });
+
+      $('.slider-dotshead').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+      });
+      
+      $('.serf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt=""></img>',
+        asNavFor: '.slider-map'
+      });
+      $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.serf-slider',
+        focusOnSelect: true
+      });
+  
+});
